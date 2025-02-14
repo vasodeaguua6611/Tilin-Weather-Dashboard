520291e0ab54d78969e7a246c867c4a0c0e58b42
vasodeaguua6611 <85977562+vasodeaguua6611@users.noreply.github.com>
2025-02-14 18:24:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def get_weather():
     else:
         return render_template('index.html')
 if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
     app.run(debug=True)
